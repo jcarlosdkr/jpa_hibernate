fc9df1dccb0354601b0c551cbc385af4828ab429
jcarlos <jcarlos.dkr@gmail.com>
2018-09-24 23:05:20 -0500

    Se crea un nuevo entityManager para cada elemento Se utiliza merge para actualizar una entidad ya no administrada y alamcenar nuevos datos

--- src/main/java/com/jc/hibernate/tests/TestEmpleados.java
+++ src/main/java/com/jc/hibernate/tests/TestEmpleados.java
@@ -9,11 +9,8 @@ import javax.persistence.Persistence;
 
 import com.jc.hibernate.modelo.Empleado;
 
-@SuppressWarnings("unused")
 public class TestEmpleados {
-	
-	
-	private static EntityManager manager;
+		
 	private static EntityManagerFactory emf;
 	
 	public static void main(String[] args){
@@ -22,25 +19,43 @@ public class TestEmpleados {
 		 * Crear el gestor de persistencia (EM).
 		 */
 		emf = Persistence.createEntityManagerFactory("aplicacion");
-		manager = emf.createEntityManager();
+		EntityManager manager = emf.createEntityManager();
+				
+		Empleado e = new Empleado(042210050L, "Alicia", "Hernández", new GregorianCalendar(1986,8,10).getTime());
+		manager.getTransaction().begin();
+		manager.persist(e);
+		manager.getTransaction().commit();
+		manager.close();
 		
+		imprimirTodo();
+		
+		manager = emf.createEntityManager();
+		manager.getTransaction().begin();
+		e.setNombre("Romina");
+		manager.merge(e);
+		manager.getTransaction().commit();
+		imprimirTodo();
+		
+	}
+
+	private static void insertInicial() {
+		EntityManager manager = emf.createEntityManager();
 		Empleado e = new Empleado(042210500L, "Juan Carlos", "Higuera Sánchez", new GregorianCalendar(1986,9,18).getTime());
-		Empleado e2 = new Empleado(042210450L, "Sarai Monserrat", "Sánchez Zamora", new GregorianCalendar(1986,8,10).getTime());
 		
 		manager.getTransaction().begin();
 		manager.persist(e);
-		manager.persist(e2);
 		manager.getTransaction().commit();
-		
-		imprimirTodo();
+		manager.close();
 	}
 
 	@SuppressWarnings("unchecked")
 	public static void imprimirTodo(){
+		EntityManager manager = emf.createEntityManager();
 		List<Empleado> emps = manager.createQuery("FROM Empleado").getResultList();
 		System.out.println("Hay " + emps.size() + " Empleados en el sistema.");
 		for(Empleado emp : emps){
 			System.out.println(emp.toString());
 		}
+		manager.close();
 	}
 }
